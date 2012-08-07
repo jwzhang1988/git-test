a6a51a84d5c07114dcbde1ef8a9a81962d812acb
He Fei <feihe.vip@gmail.com>
2012-08-07 16:20:34 +0800

    add failed test.

--- src/test/java/com/git/AppTest.java
+++ src/test/java/com/git/AppTest.java
@@ -2,10 +2,16 @@ package com.git;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class AppTest {
     @Test
     public void testName() {
         assertEquals("name", new App().getName());
+    }
+
+    @Test
+    public void should_failed() {
+        assertFalse(true);
     }
 }
